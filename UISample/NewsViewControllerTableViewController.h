384be27dee7f5d9047d81430fccc35c5aa9df6b6
shuaijun0203 <shuaijun0203@126.com>
2015-06-09 14:54:38 +0900

    add NewsModel func

---
+++ UISample/NewsViewControllerTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  NewsViewControllerTableViewController.h
+//  UISample
+//
+//  Created by guest on H27/06/09.
+//  Copyright (c) 平成27年 je.ac.jec.15cm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsViewControllerTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *newsArray;
+
+@end
