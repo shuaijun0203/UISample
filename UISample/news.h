fa33ebd2c92b23d25a4c2c2f1833c18def1239c7
shuaijun0203 <shuaijun0203@126.com>
2015-06-09 10:42:12 +0900

    add newsModel file

---
+++ UISample/news.h
@@ -0,0 +1,17 @@
+//
+//  news.h
+//  UISample
+//
+//  Created by guest on H27/06/09.
+//  Copyright (c) 平成27年 je.ac.jec.15cm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface news : NSObject
+@property (nonatomic, copy) NSString *newsIcon;
+@property (nonatomic, copy) NSString *newsURL;
+@property (nonatomic, copy) NSString *newsDescription;
+@property (nonatomic, copy) NSString *newsTitle;
+@property (nonatomic, assign) NSDate *pubDate;
+@end
---
+++ UISample/news.m
@@ -0,0 +1,13 @@
+//
+//  news.m
+//  UISample
+//
+//  Created by guest on H27/06/09.
+//  Copyright (c) 平成27年 je.ac.jec.15cm. All rights reserved.
+//
+
+#import "news.h"
+
+@implementation news
+
+@end
