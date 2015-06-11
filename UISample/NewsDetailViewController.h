153dc6568a4f4e551127df2f8e7456d404b2cb3e
shuaijun0203 <shuaijun0203@126.com>
2015-06-11 14:36:31 +0900

    add news detail functions

---
+++ UISample/NewsDetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  NewsDetailViewController.h
+//  UISample
+//
+//  Created by guest on H27/06/10.
+//  Copyright (c) 平成27年 je.ac.jec.15cm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsDetailViewController : UIViewController
+@property (nonatomic, strong) NSMutableArray *newsTitle;
+@property (nonatomic, strong) NSMutableArray *newsDate;
+@property (nonatomic, strong) NSMutableArray *news;
+@property (nonatomic, strong) NSMutableArray *newsSource;
+
+
+@end
